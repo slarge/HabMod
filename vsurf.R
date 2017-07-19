fd8bc814b461ca31fb7188f70b4719207cfde1a5
Scott Large <slarge@users.noreply.github.com>
2017-07-19 12:21:56 +0200

    make sure that factors are factors

--- vsurf.R
+++ vsurf.R
@@ -1,32 +1,45 @@
 rm(list = ls())
 
+strt_time <- Sys.time()
+
 # Load libraries
 library(VSURF)
-library(snow)
-library(Rmpi)
+#library(snow)
+#library(Rmpi)
+library(parallel)
+library(doParallel)
 
 false<-F
 true<-T
-removeZero<-false
 
 #parameters
 x = "x.txt"
-x = read.table(x, header - T, sep = ",") # for test
+x = read.table(x, header = T, sep = ",") # for test
 y = "y.txt"
-y = read.table(y, header - T, sep = ",") # for test
+y = read.table(y, header = T, sep = ",") # for test
 ntree = 500 
-mtry = max(floor(ncol(x)/3), 1)
+mtry = max(floor(ncol(x)/3), 1);
 nfor.thres = 50
 nmin = 1
 nfor.interp = 25 
 nsd = 1
 nfor.pred = 25
 parallel = false 
-ncores = detectCores() - 1
-clusterType = "PSOCK"
+ncores = detectCores() - 1;
+clusterType = "PSOCK";
 seed = 627
 
-strt_time <- Sys.time()
+if(ncol(y) == 1) {
+  y = y[,1]
+}
+
+
+if(length(unique(y)) == 2) {
+  y = as.factor(y)
+}
+
+
+
 cluster <- makeCluster(ncores) # convention to leave 1 core for OS
 registerDoParallel(cluster)
 
