cd5b114af4b0dc20445e33b276b409e9277812a8
Scott Large <slarge@users.noreply.github.com>
2017-07-19 11:39:05 +0200

    update with x and y txt

--- vsurf.R
+++ vsurf.R
@@ -5,21 +5,30 @@ library(VSURF)
 library(snow)
 library(Rmpi)
 
+false<-F
+true<-T
+removeZero<-false
+
 #parameters
-data("toys")
-x = toys$x; # for test
-y = toys$y; # for test
-ntree = 500; 
-mtry = max(floor(ncol(x)/3), 1);
-nfor.thres = 50;
-nmin = 1;
-nfor.interp = 25; 
-nsd = 1;
-nfor.pred = 25;
-parallel = FALSE; 
-ncores = detectCores() - 1;
-clusterType = "PSOCK";
-seed = 627;
+x = "x.txt"
+x = read.table(x, header - T, sep = ",") # for test
+y = "y.txt"
+y = read.table(y, header - T, sep = ",") # for test
+ntree = 500 
+mtry = max(floor(ncol(x)/3), 1)
+nfor.thres = 50
+nmin = 1
+nfor.interp = 25 
+nsd = 1
+nfor.pred = 25
+parallel = false 
+ncores = detectCores() - 1
+clusterType = "PSOCK"
+seed = 627
+
+strt_time <- Sys.time()
+cluster <- makeCluster(ncores) # convention to leave 1 core for OS
+registerDoParallel(cluster)
 
 dat.vsurf <- VSURF::VSURF(x = x,
                           y = y,
@@ -32,7 +41,10 @@ dat.vsurf <- VSURF::VSURF(x = x,
                           nfor.pred = nfor.pred,
                           parallel = parallel, 
                           ncores = ncores,
-                          clusterType = clusterType);
+                          clusterType = clusterType)
+                          
+stopCluster(cluster)
+registerDoSEQ()
 
-output_file <- "output.rds";
-saveRDS(dat.surf, output_file)
+output_file <- "output.rds"
+saveRDS(dat.vsurf, output_file)
