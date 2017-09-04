abb821bb4883db45b7daca7680a633de093124c7
slarge <largesi@gmail.com>
2017-09-04 10:58:39 +0200

    better pointers to namespace

--- analysis/data/raw_data/load_new_data.R
+++ analysis/data/raw_data/load_new_data.R
@@ -2,12 +2,12 @@
 
 # need to do this: https://github.com/tidyverse/googledrive/issues/79
 options(httr_oob_default=TRUE)
-drive_auth()
+googledrive::drive_auth()
 
 drive_path <- "~/hab_mod/data/"
 data_path <- "analysis/data/raw_data/"
 
-drive_files <-  drive_ls(drive_path, pattern = "spring.data.RData")
+drive_files <-  googledrive::drive_ls(drive_path, pattern = "spring.data.RData")
 
-drive_download(file = drive_files,
+googledrive::drive_download(file = drive_files,
                path = paste0(data_path, drive_files$name))
