c0dd97756ea84cdd57df6790ffb421d9169c82af
slarge <largesi@gmail.com>
2017-11-03 09:35:02 +0100

    move plotting stuff to new script

--- analysis/procedure/modelplots.R
+++ analysis/procedure/modelplots.R
@@ -1,3 +1,237 @@
+rm(list = ls())
+#-------------------------------------------------------------------------------
+#Requiredpackages
+library(randomForest)
+library(dplyr)
+library(viridis)
+# devtools::install_github('tidyverse/ggplot2')
+library(ggplot2)
+# library(scales)
+library(ggthemes)
+# devtools::install_github("dgrtwo/gganimate")
+library(gganimate)
+# library(RFinfer)
+# devtools::install_github("swager/randomForestCI")
+# library(randomForestCI)
+library(pdp)
+set.seed(627)
+
+#-------------------------------------------------------------------------------
+raw_path <- "analysis/data/raw_data/"
+derived_path <- "analysis/data/derived_data/"
+fig_path <- "analysis/figures/"
+#-------------------------------------------------------------------------------
+
+# couple of functions
+partial_overview <- function(object, pred.var) {
+  
+  partial_output <- pdp::partial(object, pred.var, chull = FALSE)
+  colnames(partial_output) <- c("x", "y")
+  partial_output$pred.var <- pred.var
+  return(partial_output)
+}
+
+
+gggsave <- function(filename, plot, ...) {
+  ggsave(filename, plot, ...)
+  
+  if(grepl("fall", filename)) sns <- "fall"
+  if(grepl("spring", filename)) sns <- "spring"
+  
+  gd_path <- googledrive::as_dribble(file.path("temp_for_scott/analysis/output", sns))
+  googledrive::drive_upload(media = filename, path = gd_path, verbose = TRUE)
+  
+}
+
+
+
+season_list <- c("fall", "spring")
+sp.svspp <- c(22, 28, 73, 74, 105, 107, 141)
+season = "fall"
+# sp.i = 73
+mod_type = "BM"
+
+for(season in season_list) {
+
+  for(sp.i in sp.svspp){
+    
+    sp_data <- readRDS(paste0(raw_path,
+                              grep(paste0("biomass_habmod-", season, ".*.rds"),
+                                   list.files(raw_path),
+                                   value = TRUE))) %>%
+      dplyr::filter(SVSPP == sp.i) %>%
+      na.omit
+    # 
+    name <- unique(sp_data$name)
+    
+    files <- grep(paste(unique(sp_data$name), collapse = "|"),
+                  list.files(paste0(derived_path, season, "/"), 
+                             pattern = "model.rds"), value = TRUE)
+    
+    run.rf <- readRDS(paste0(derived_path, season, "/", grep(paste0(name,"-", season, "-", mod_type, "-RF"),
+                                                             files, value = TRUE)))
+    
+    pred_vars <- run.rf$finalModel$xNames
+
+    pd <- purrr::map(pred_vars, function(x) partial_overview(object = run.rf, pred.var = x)) %>% bind_rows()
+    
+    pd2 <- pd %>% 
+      group_by(pred.var) %>% 
+      summarize(TOTAL = sum(y),
+                MAX = max(y)) %>% 
+      arrange(-MAX, -TOTAL) %>% 
+      mutate(pred.var = factor(pred.var, pred.var))
+      
+    pd$pred.var <- factor(pd$pred.var, levels =   levels(pd2$pred.var))  
+    
+    map_title <- case_when(mod_type == "BO" ~ "Biomass only model",
+                           mod_type == "BM" ~ "Biomass with occupancy model",
+                           mod_type == "PA" ~ "Occupancy model")
+    
+    map_legend <- case_when(mod_type %in% c("BO", "BM") ~ "Biomass (log) in tons",
+                            mod_type == "PA" ~ "Probability of presence")
+    
+    ceiling(length(unique(pd$pred.var))/4)
+    
+    pd_plot <- ggplot(pd) + 
+      # geom_area(aes(x = x, y = y), color = "grey20", alpha = .2) +
+      geom_line(aes(x = x, y = y), color = "black") +
+      facet_wrap(~ pred.var, scales = "free_x", strip.position = "top", ncol = 4) +
+      theme_bw() +
+      labs(subtitle = paste0(gsub("_", " ", name), " - ", map_title),
+           # color = "",
+           y = map_legend,
+           x = "") +
+      theme(legend.position = "bottom",
+            strip.background = element_blank(), 
+            legend.key.width = unit(0.5, "cm"))
+    
+    gggsave(filename = paste0(fig_path, season, "/", name,"-", season, "-partial_plot.png"),
+            plot = pd_plot,
+            width = 8,
+            height = ifelse(ceiling(length(unique(pd$pred.var))/4) > 4,
+                            10,
+                            6))
+    
+  } # close species loop
+} # close season loop
+  
+  # autoplot(pd, contour = FALSE, main = "ggplot2 version", 
+  #          legend.title = "Partial\ndependence") +
+  #   facet_wrap(.~ pred.var)
+  
+    # if(mod_type %in% c("BO", "BM")){
+      
+      # if(mod_type == "BM"){
+      #   PA_rf <- readRDS(paste0(derived_path, season, "/", grep(paste0(name, "-", "PA-RF"),
+      #                                                           files, value = TRUE)))
+      #   
+      #   sp_data$PRESPROB <- predict(PA_rf, newdata = sp_data, type = "prob")[, 2]
+      #   rm(PA_rf)
+      # }
+      # 
+      # res_var <- dplyr::case_when(mod_type %in% c("BM", "BO") ~ "BIOMASS",
+      #                             mod_type == "PA" ~ "PRESENCE",
+      #                             TRUE ~ NA_character_)
+      # 
+      # rf_data <- sp_data[, c(res_var, pred_vars, "LAT", "LON")]
+      # 
+      # # if(mod_type %in% c("BM", "BO")){
+      # rf_data <- rf_data[rf_data$BIOMASS != 0,]
+      # # }
+      # 
+      # rf_data <- Filter(var, rf_data)
+      # 
+      # rf_selection <- caret::createDataPartition(y = rf_data[, res_var],
+      #                                            p = 0.75,
+      #                                            list = FALSE)
+      # 
+      # rf_train <- rf_data[rf_selection, ]
+      # rf_test <- rf_data[-rf_selection, ]
+      # 
+      # rf_trainX <- rf_train[, names(rf_train) %in% pred_vars]
+      # rf_trainY <- rf_train[, res_var]
+      # 
+      # # if(mod_type == "PA"){
+      # #   rf_trainY <- as.factor(rf_trainY)
+      # # }
+      # 
+      # strt_time <- Sys.time()
+      # cluster <- parallel::makeCluster(parallel::detectCores() - 1)
+      # doParallel::registerDoParallel(cluster)
+      # 
+      # rf_run <- randomForest::randomForest(x = rf_trainX, y = rf_trainY,
+      #                                      mtry = run.rf$finalModel$mtry,
+      #                                      ntree = 5000,
+      #                                      keep.inbag = TRUE,
+      #                                      importance = TRUE,
+      #                                      replace = TRUE)
+      # 
+      # parallel::stopCluster(cluster)
+      # foreach::registerDoSEQ()
+      # 
+      # stp_time <- Sys.time()
+      # stp_time - strt_time
+      # 
+      # # 
+      # rf_test$PREDICTION <- predict(rf_run,  rf_test)
+      # rf_test$RESID <- rf_test$BIOMASS - rf_test$PREDICTION
+      # # 
+      # 
+      # RMSE_test <- sqrt(sum((rf_test$BIOMASS - rf_test$PREDICTION)^2)/length(rf_test$PREDICTION))
+      # rRMSE_test <- round(RMSE_test/(max(rf_test$BIOMASS) - min(rf_test$BIOMASS)), 2)
+      # 
+      # var_hat <- randomForestCI::randomForestInfJack(rf_run, rf_test[, -1], calibrate = TRUE)
+      # 
+      # # 
+      # df <- data.frame(BIOMASS = rf_test$BIOMASS, PREDICTED = var_hat$y.hat, error = var_hat$var.hat)
+      # df <- df %>%
+      #   dplyr::mutate(ci_upper = PREDICTED + (1.96 * sqrt(error)),
+      #                 ci_lower = PREDICTED - (1.96 * sqrt(error)))
+      # 
+      # ## Predicted/observed plot
+      # p1 <- ggplot(df, aes(x = BIOMASS, y = PREDICTED))
+      # p1 <- p1 +
+      #   geom_linerange(aes(ymin = ci_lower,
+      #                      ymax = ci_upper),
+      #                  color = "black", alpha = 0.2) +
+      #   geom_point(color = "black", alpha = 0.8, shape = 16) +
+      #   geom_abline(intercept=0, slope = 1, linetype = 2) +
+      #   labs(x = "Observed biomass",
+      #        y = "Predicted biomass",
+      #        title = sprintf("%s (%s)", gsub("_", " ", name), season),
+      #        subtitle = paste0("The error (relative RMSE) is about ", rRMSE_test * 100, "% \nas large as the mean biomass.")) +
+      #   coord_equal() +
+      #   theme_bw()
+      ggsave(paste0(fig_path, name,"-", season, "-fit_plot.png"), p1, width = 5, height = 5)
+      
+      ## Importance plot
+      im <- caret::varImp(run.rf)
+      
+      stackedData <- caret:::sortImp(im, top =  dim(im$importance)[1])
+      
+      featureNames <- rownames(stackedData)
+      outcomeNames <- colnames(stackedData)
+      featureNames <- gsub("_0d$", "", featureNames)
+      
+      stackedData$Feature <- factor(rep(featureNames, length(outcomeNames)), 
+                                    levels = rev(featureNames))
+      names(stackedData) <- c("Importance", "Feature")
+      
+      p4 <- ggplot(stackedData, aes(x = Feature, y = Importance)) +
+        geom_segment(aes(x = Feature, xend = Feature,
+                         y = 0, yend = Importance)) +
+        geom_point(stat = "identity", aes(y = Importance), color = "grey50") +
+        coord_flip() +
+        labs(x = "",
+             y = "Importance (scaled)",
+             title = gsub("_", " ", name)) +
+        theme_bw()
+      
+      ggsave(paste0(fig_path, name, "-", season, "-importance_plot.png"), p4, width = 5, height = 5)
+      
+
+
 
 
 ## The stuff below is used for plotting, and model validation
