3250a33b23a531ae78cc5e5981545367d67ee995
slarge <largesi@gmail.com>
2017-11-23 11:40:36 +0100

    add the new six char sp. abbreviations

--- analysis/data/raw_data/load_new_data.R
+++ analysis/data/raw_data/load_new_data.R
@@ -68,3 +68,32 @@ climDirs <- climDirs[!grepl("fall_raster/clim", climDirs)]
 lapply(climDirs, function(x) gd_loader(gdDir = "temp_for_scott/1_habitat_analysis_2017/",
                                       subDir = x, 
                                       mainDir = "analysis/data/raw_data"))
+
+
+
+SVSPP <- read.csv("analysis/data/raw_data/SVSPP.csv")
+species_list <- c(101,102,103,104,105,
+                  106,107,108,109,112,
+                  121,13,131,135,139,
+                  14,141,143,145,15,
+                  151,155,156,163,164,
+                  168,171,172,176,177,
+                  22,23,24,25,26,
+                  27,28,32,33,34,
+                  35,36,69,72,73,
+                  74,75,76,77,78,84)
+SVSPP %>% 
+  filter(SVSPP %in% species_list) %>%
+  mutate(COMNAME = tolower(COMNAME)) %>% 
+  tidyr::extract(COMNAME, into=c('partA', 'partB'), '(.*)\\s+([^ ]+)$', remove = FALSE) %>% 
+  mutate(partA = substr(partA, start = 1, stop = 3),
+         partB = substr(partB, start = 1, stop = 3),
+         partC = ifelse(is.na(partA) | is.na(partB),
+                    substr(COMNAME, start = 1, stop = 6),
+                    paste0(partA, partB)),
+         six_names = stringr::str_pad(partC, 6, pad = "z", side = "right")) %>% 
+  dplyr::select(-partA,
+         -partB,
+         -partC) %>% 
+  write.csv("analysis/data/raw_data/SVSPP_abbr.csv", row.names = FALSE)
+
